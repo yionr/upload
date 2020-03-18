9902bdd58aa9b0c990988c2b1cd8b2a35e90787b
Yionr <1475829476@qq.com>
2020-03-18 14:51:33 +0800

    - 主页内容调整 - 上传容量重新限制1G - 添加数据库(resources/db.properties)

---
+++ src/main/java/cloud/yionr/entity/User.java
@@ -0,0 +1,31 @@
+package cloud.yionr.entity;
+
+//映射数据库中的每个用户,数据库里面保存每个name 以及对应的id
+public class User {
+    private String name;
+    private String id;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", id='" + id + '\'' +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
