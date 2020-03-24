147f8d9ac8b0343e23f8133407f32745356a4148
yionr <1475829476@qq.com>
2020-03-24 20:18:21 +0800

    - 增加一次校验，之前用户可以通过一个特定的流程自定义学号和姓名，这一道校验彻底关闭违规文件名上传

--- src/main/java/cloud/yionr/controller/UploadController.java
+++ src/main/java/cloud/yionr/controller/UploadController.java
@@ -1,7 +1,12 @@
 package cloud.yionr.controller;
 
+import cloud.yionr.Exception.IdNotMatchException;
+import cloud.yionr.Exception.StudentNotFoundException;
 import cloud.yionr.Exception.SysException;
+import cloud.yionr.entity.Student;
+import cloud.yionr.service.StudentService;
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,45 +17,70 @@ import java.io.File;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-//TODO 将文件根据第几次上传分类,实现所有用户所有作业归档,然后后期做一个可以查看历史上传的功能
+//TODO 做一个可以查看历史上传的功能
 @Controller
 public class UploadController {
+    @Autowired
+    StudentService studentService;
 
     private Logger logger = Logger.getLogger(UploadController.class);
 
     @RequestMapping("/uploadHomework")
-    public String UploadGroupByWeek(MultipartFile file, HttpServletRequest req, @RequestParam("fileName") String fileName) throws SysException {
+    public String UploadGroupByWeek(MultipartFile file, HttpServletRequest req, @RequestParam("fileName") String fileName) throws SysException, StudentNotFoundException, IdNotMatchException {
 
         logger.info("try to submit a file>>> ip: " + req.getRemoteAddr() + ">>>fileName: " + fileName);
+//        到这儿，肯定是学号+姓名的形式了，学号存在2位和11位的情况 11位两个学号是确定的，2位可以是随意两位
+        String fileName_suf = fileName.split("\\.")[0];
+//        根据fileName获取id
+        String id = fileName_suf.split("\\D+$")[0];
+//        获取name    1好像和split的特性有关，暂时不是很清楚
+        String name = fileName_suf.split("^\\d+")[1];
 
-        //获取当前周（相对于开学）
-        LocalDateTime now = LocalDateTime.now();
-        LocalDateTime baseDate = LocalDateTime.of(2020,3,1,0,0);
-//        这里加上1 ： 原本是21-1 = 20 /7 = 2 导致21号归档到第三周 而22号归档到第四周，加上一偏移之后，周次正确
-        int weekNum = (now.getDayOfYear() + 1 - baseDate.getDayOfYear())/7 + 1;
-        //创建当前周的文件夹
-//        File CurrentWeekDir = new File(req.getServletContext().getRealPath("/WEB-INF/homeWork/第" + weekNum + "周作业"));
-        File CurrentWeekDir = new File("/root/homeWork/" + weekNum);
-        if (!CurrentWeekDir.exists())
-            CurrentWeekDir.mkdirs();
-        //创建作业
-        //FIXME 可能会出现前缀相同但是后缀不同的情况，但是这种情况即使提交了，也不会覆盖原文件，阻止用户二次提交相同文件的本意是防止被别的用户乱搞
-        File homeWork = new File(CurrentWeekDir,fileName);
-        //TODO 用户拥有纠错的机会，重新上传，遇到同名文件时，比较两个文件大小，提供文件修改日期并提醒用户是否替换
-        if (homeWork.exists())
-            throw new SysException("服务器上已经存在此作业!");
-        try {
-            homeWork.createNewFile();
-            logger.info("serverFileCreated!");
-            file.transferTo(homeWork);
-            logger.info("serverFileTransferSuccess!");
-            return "success";
-        } catch (IOException e) {
-            e.printStackTrace();
-            throw new SysException("在服务器上创建文件时遇到未知错误，上传失败!");
+        Student student = studentService.FindByName(name);
+        logger.info("fileName_suf: " + fileName_suf + " id: " + id + " name: " + name + "   searched: " + student);
+        if (!(student == null)) {
+//            这里要分两种情况，学号2位和11位
+            if (!student.getId().equals(id) && !student.getId().substring(9).equals(id)) {
+                throw new IdNotMatchException("姓名学号不匹配，请重试！");
+            } else {
+                //获取当前周（相对于开学）
+
+                LocalDateTime now = LocalDateTime.now();
+                LocalDateTime baseDate = LocalDateTime.of(2020, 3, 1, 0, 0);
+//              这里加上1 ： 原本是21-1 = 20 /7 = 2 导致21号归档到第三周 而22号归档到第四周，加上一偏移之后，周次正确
+                int weekNum = (now.getDayOfYear() + 1 - baseDate.getDayOfYear()) / 7 + 1;
+                //创建当前周的文件夹
+                File CurrentWeekDir = new File("/root/homeWork/" + weekNum);
+                //测试环境下用下面地址
+//                File CurrentWeekDir = new File("/Users/Yionr/homeWork/" + weekNum);
+                if (!CurrentWeekDir.exists())
+                    CurrentWeekDir.mkdirs();
+                //创建作业
+                //FIXME 可能会出现前缀相同但是后缀不同的情况，但是这种情况即使提交了，也不会覆盖原文件，阻止用户二次提交相同文件的本意是防止被别的用户乱搞
+                File homeWork = new File(CurrentWeekDir, fileName);
+                //TODO 用户拥有纠错的机会，重新上传，遇到同名文件时，比较两个文件大小，提供文件修改日期并提醒用户是否替换
+                if (homeWork.exists())
+                    throw new SysException("服务器上已经存在此作业!");
+                try {
+                    homeWork.createNewFile();
+                    logger.info("serverFileCreated!");
+                    file.transferTo(homeWork);
+                    logger.info("serverFileTransferSuccess!");
+                    return "success";
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    throw new SysException("在服务器上创建文件时遇到未知错误，上传失败!");
+                }
+            }
         }
-
+        else{
+            throw new StudentNotFoundException("数据库中无"+ name + "的记录！");
+        }
     }
 
 }
+
+//TODO 虽然revise那里已经做过一次校正了，但是有一种情况，用户是可以避免校验的。所以应该在这里再加一层校验，通过才能保存。这样就彻底防止学号不匹配姓名的情况了
