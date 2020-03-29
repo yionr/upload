96f21079f4094598d1184c0733aa97a23b3d3079
yionr <1475829476@qq.com>
2020-03-29 13:37:17 +0800

    添加 根据学号查找，姓名错误的返回提示 修改 当用户填入数据错误的时候，服务器返回内容，将有不同的key标识

--- src/main/java/cloud/yionr/controller/ReviseController.java
+++ src/main/java/cloud/yionr/controller/ReviseController.java
@@ -27,21 +27,21 @@ public class ReviseController {
         DayOfWeek weekDay = dateTool.getWeekDay();
         int timeOfHour = dateTool.getHour();
 
-        if (weekDay == DayOfWeek.FRIDAY) {
-            if (timeOfHour >= 12) {
-                logger.info("名为:  " + oFName + " 的文件试图在禁止上传的时间点上传文件，已拦截");
-                return "over";
-            }
-        }
-        if (weekDay == DayOfWeek.SATURDAY || weekDay == DayOfWeek.SUNDAY || weekDay == DayOfWeek.MONDAY){
-            logger.info("名为:  " + oFName + " 的文件试图在禁止上传的时间点上传文件，已拦截");
-            return "over";
-        }
-        if (weekDay == DayOfWeek.TUESDAY)
-            if (timeOfHour < 8){
-                logger.info("名为:  " + oFName + " 的文件试图在禁止上传的时间点上传文件，已拦截");
-                return "over";
-            }
+//        if (weekDay == DayOfWeek.FRIDAY) {
+//            if (timeOfHour >= 12) {
+//                logger.info("名为:  " + oFName + " 的文件试图在禁止上传的时间点上传文件，已拦截");
+//                return "over";
+//            }
+//        }
+//        if (weekDay == DayOfWeek.SATURDAY || weekDay == DayOfWeek.SUNDAY || weekDay == DayOfWeek.MONDAY){
+//            logger.info("名为:  " + oFName + " 的文件试图在禁止上传的时间点上传文件，已拦截");
+//            return "over";
+//        }
+//        if (weekDay == DayOfWeek.TUESDAY)
+//            if (timeOfHour < 8){
+//                logger.info("名为:  " + oFName + " 的文件试图在禁止上传的时间点上传文件，已拦截");
+//                return "over";
+//            }
 
         logger.info("开始尝试文件名纠正,原文件名称为: " + oFName);
 
@@ -51,10 +51,10 @@ public class ReviseController {
             //如果学生输入的学号和正确的学号不一致，则返回正确学号，提醒用户
             if (id.length() == 11)
                 if (!student.getId().equals(id))
-                    return student.getId();
+                    return "correctId:" + student.getId();
             if (id.length() == 2)
                 if (!student.getId().substring(9,11).equals(id))
-                    return student.getId().substring(9,11);
+                    return "correctId:" + student.getId().substring(9,11);
         }
 //        如果姓名无法识别，则根据学号来查找
         else{
@@ -66,8 +66,13 @@ public class ReviseController {
 //        如果到这还没有找到的话，就说明，这个文件名，不是以学号开头，也不是以姓名结尾的，暂时无法处理
         if (student == null)
             return "false";
-
-//        能到这里，表示找到了学生，并且它的学号没有填错
+        else{
+//        到这里，表示根据学号找到了学生,但是name错了
+            if (!student.getName().equals(name)){
+                return "correctName:" + student.getName();
+            }
+        }
+//        到这里，代表格式完全正确
 //        如果是16级的学生
         if (student.getId().startsWith("2016"))
             return student.getId() + student.getName();
