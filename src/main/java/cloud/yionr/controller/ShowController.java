42b011acea52315574e869f73ad0a17b3011713f
yionr <1475829476@qq.com>
2020-03-29 14:45:05 +0800

    emmmm。。。

--- src/main/java/cloud/yionr/controller/ShowController.java
+++ src/main/java/cloud/yionr/controller/ShowController.java
@@ -1,49 +1,18 @@
 package cloud.yionr.controller;
 
-import cloud.yionr.common.DateTool;
+import cloud.yionr.common.ServerFileTool;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
-import java.io.File;
-import java.io.IOException;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Properties;
-
 @RestController
 public class ShowController {
-
     @Autowired
-    DateTool dateTool;
-
-    Properties properties;
-    {
-        properties = new Properties();
-        try {
-            properties.load(UploadController.class.getResourceAsStream("/common.properties"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+    ServerFileTool serverFileTool;
 
     @RequestMapping("/show")
     public String show(){
-
-        //根据得到的周次去服务器里面查询指定文件夹，如果不存在（指定周还没有人上传，但是有人查询）则创建文件夹
-//        File file = new File("/root/homeWork/" + dateTool.getWeek());
-//        测试专用地址
-        File file = new File(properties.getProperty("homeWorkRoot") , dateTool.getWeek()+"");
-        if (!file.exists())
-            file.mkdirs();
-        //不断读文件夹里面的文件,每读一个提取文件名放入数组
-        File[] files = file.listFiles();
-        ArrayList<String> arrayList = new ArrayList<>();
-        for (File fileItem :
-                files) {
-            arrayList.add(fileItem.getName());
-        }
-        return arrayList.toString();
+        System.out.println(serverFileTool.getFileListWithSuf().toString());
+        return serverFileTool.getFileListWithSuf().toString();
     }
 }
