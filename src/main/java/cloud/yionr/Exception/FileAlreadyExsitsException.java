252e658dfee2cd232f457eac9cc76d95bd75ed13
yionr <1475829476@qq.com>
2020-03-29 14:43:01 +0800

    添加了一个服务器上已经存在文件的异常类

---
+++ src/main/java/cloud/yionr/Exception/FileAlreadyExsitsException.java
@@ -0,0 +1,18 @@
+package cloud.yionr.Exception;
+
+public class FileAlreadyExsitsException extends Exception {
+    private String message;
+
+    public FileAlreadyExsitsException(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
