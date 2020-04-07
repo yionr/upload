359c3e4fb95cde1418e0019623eeae9c2facf518
yionr <1475829476@qq.com>
2020-04-07 08:51:41 +0800

    去除getLogger方法，此方法会导致报错

--- src/main/java/cloud/yionr/common/Log4jUtils.java
+++ src/main/java/cloud/yionr/common/Log4jUtils.java
@@ -2,16 +2,13 @@ package cloud.yionr.common;
 
 import cloud.yionr.controller.UploadController;
 import org.apache.commons.io.IOUtils;
-import org.apache.log4j.Logger;
 import org.springframework.stereotype.Component;
-import sun.reflect.Reflection;
 
 import java.io.*;
 import java.util.Properties;
 
 @Component
 public class Log4jUtils {
-    private Logger logger = null;
     private File logFile;
     {
         Properties properties = new Properties();
@@ -30,13 +27,6 @@ public class Log4jUtils {
                 e.printStackTrace();
             }
         }
-    }
-
-    public Logger getLogger(){
-        if (logger == null){
-            logger = Logger.getLogger(Reflection.getCallerClass().getName());
-        }
-        return logger;
     }
     public void addVisitCount(){
         int count;
