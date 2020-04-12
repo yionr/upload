3f9cbe40649b9f02c65ce7f458b8d441889980c2
yionr <1475829476@qq.com>
2020-04-12 19:53:10 +0800

    移除一个面板的动作

--- web/js/general.js
+++ web/js/general.js
@@ -1,27 +1,18 @@
 function roll(e) {
     let pad = e.parentNode;
     let offsetWidth = pad.offsetWidth;
-    if (pad.className === 'tipPad'){
-        let offsetRight = pad.style.right;
-        if (offsetRight != '-150px'){
-            pad.style.right = - offsetWidth + "px";
-            e.innerText = '展开';
-        }
-        else if (offsetRight == '-150px'){
-            pad.style.right = 0 + "px";
-            e.innerText = '收起';
-        }
+
+    let offsetRight = pad.style.right;
+
+    if (offsetRight != '-150px'){
+        pad.style.right = - offsetWidth + "px";
+        e.innerText = '展开';
     }
-    else{
-        let offsetLeft = pad.offsetLeft;
-        if (offsetLeft === 0) {
-            pad.style.left = -offsetWidth + "px";
-            e.innerText = '展开';
-        }
-        else if (offsetLeft === -150){
-            pad.style.left = 0 + "px";
-            e.innerText = '收起';
-        }
+    else if (offsetRight == '-150px'){
+        pad.style.right = 0 + "px";
+        e.innerText = '收起';
     }
 }
+
+
 
