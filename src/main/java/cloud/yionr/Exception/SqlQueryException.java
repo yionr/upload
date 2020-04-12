d0fd3509c27e59477747603f5a88b94181638c68
yionr <1475829476@qq.com>
2020-04-12 13:58:06 +0800

    新增异常类，用于处理数据库查询时，出现的任何异常

---
+++ src/main/java/cloud/yionr/Exception/SqlQueryException.java
@@ -0,0 +1,18 @@
+package cloud.yionr.Exception;
+
+public class SqlQueryException extends Exception {
+    private String message;
+
+    public SqlQueryException(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
