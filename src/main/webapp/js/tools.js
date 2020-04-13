7c44568550579c5bbcd9ea62d470f5e6e554ca62
yionr <1475829476@qq.com>
2020-04-13 12:38:21 +0800

    添加用户使用设备的简单判定（移动设备 or pc）

--- src/main/webapp/js/tools.js
+++ src/main/webapp/js/tools.js
@@ -33,6 +33,15 @@ function isWorkingDay() {
             return false;
     return true;
 }
+function isPC(){
+    let userAgentInfo = navigator.userAgent;
+    let Agents = ["Android", "iPhone", "SymbianOS", "Windows Phone", "iPad", "iPod"];
+    let flag = true;
+    for (let v = 0; v < Agents.length; v++) {
+        if (userAgentInfo.indexOf(Agents[v]) > 0) { flag = false; break; }
+    }
+    return flag;
+}
 
 function openUploadEntrance(){
     let uploadArea = document.getElementsByClassName("uploadArea")[0];
