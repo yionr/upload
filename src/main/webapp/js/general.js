ef02984f3ba463c52962c737309a59aa124924cb
yionr <1475829476@qq.com>
2020-04-13 12:38:42 +0800

    更严谨

--- src/main/webapp/js/general.js
+++ src/main/webapp/js/general.js
@@ -1,14 +1,11 @@
 function roll(e) {
     let pad = e.parentNode;
     let offsetWidth = pad.offsetWidth;
-
-    let offsetRight = pad.style.right;
-
-    if (offsetRight != '-150px'){
+    if (pad.style.right !== '-150px'){
         pad.style.right = - offsetWidth + "px";
         e.innerText = '展开';
     }
-    else if (offsetRight == '-150px'){
+    else if (pad.style.right === '-150px'){
         pad.style.right = 0 + "px";
         e.innerText = '收起';
     }
